fad813d6675a61dc3238b46b2c2d8e3f2e28db5e
Salvador <sfandino@yahoo.com>
2011-06-01 18:57:35 +0200

    working on it

--- lib/AnyEvent/Pg.pm
+++ lib/AnyEvent/Pg.pm
@@ -1,47 +1,182 @@
 package AnyEvent::Pg;
 
-use 5.010001;
+our $VERSION = 0.01;
+
+use 5.010;
 use strict;
 use warnings;
+use Carp;
 
-require Exporter;
+use AnyEvent;
+use Pg::PQ qw(:pgres_polling);
 
-our @ISA = qw(Exporter);
+sub _check_state {
+    my $self = shift;
+    my $state = $self->{state};
+    for (@_) {
+        return if $_ eq $state;
+    }
+    my $upsub = (caller 1)[3];
+    croak "$upsub can not be called in state $state";
+}
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use AnyEvent::Pg ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
-our $VERSION = '0.01';
+sub _ensure_list { ref $_[0] ? @{$_[0]} : $_[0] }
 
 
-# Preloaded methods go here.
+sub new {
+    my ($class, $conninfo, %opts) = @_;
+
+    my $on_connect = delete $opts{on_connect};
+    my $on_connect_error = delete $opts{on_connect_error};
+    my $on_empty_queue = delete $opts{on_empty_queue};
+
+    my $dbc = Pg::PQ::Conn->start($conninfo);
+    # FIXME: $dbc may be undef
+    my $self = { state => 'connecting',
+                 dbc => $dbc,
+                 on_connect => $on_connect,
+                 on_connect_error => $on_connect_error,
+                 queries => [],
+               };
+    bless $self, $class;
+
+    $self->_connectPoll;
+
+    $self;
+}
+
+sub dbc { shift->{dbc} }
+
+sub _connectPoll {
+    my $self = shift;
+    my $dbc = $self->{dbc};
+    my $fd = $self->{fd};
+
+    say "_connectPoll";
+
+    my $r;
+    if (defined $fd) {
+        $r = $dbc->connectPoll;
+    }
+    else {
+        $fd = $self->{fd} = $dbc->socket;
+        $r = PGRES_POLLING_WRITING;
+    }
+
+    say $r;
+
+    my $goto;
+    my $rw;
+    my $ww;
+
+    given ($r) {
+        when (PGRES_POLLING_READING) {
+            $rw = $self->{read_watcher} // AE::io $fd, 0, sub { $self->_connectPoll };
+            say "fd: $fd, read_watcher: $rw";
+        }
+        when (PGRES_POLLING_WRITING) {
+            $ww = $self->{write_watcher} // AE::io $fd, 1, sub { say "can write"; $self->_connectPoll };
+            say "fd: $fd, write_watcher: $ww";
+        }
+        when (PGRES_POLLING_FAILED) {
+            $goto = '_on_connect_error';
+        }
+        when (PGRES_POLLING_OK) {
+            $goto = '_on_connect';
+        }
+    }
+    $self->{read_watcher} = $rw;
+    $self->{write_watcher} = $ww;
+    no warnings 'uninitialized';
+    say "read_watcher: $rw, write_watcher: $ww";
+
+    $self->$goto if $goto;
+}
+
+sub _call_back {
+    my $cb = shift;
+    $cb->(@_) if $cb;
+}
+
+sub _on_connect {
+    my $self = shift;
+    my $dbc = $self->{dbc};
+    $dbc->nonBlocking(1);
+    $self->{state} = 'connected';
+    _call_back($self->{on_connect}, $self);
+    $self->_on_push_query;
+}
+
+sub _on_connect_error {
+    my $self = shift;
+    $self->{state} = 'failed';
+}
+
+sub push_query {
+    my $self = shift;
+    $self->_check_state('connecting', 'connected');
+    push @{$self->{queries}}, { @_ };
+    $self->_on_push_query if $self->{state} eq 'connected';
+}
+
+sub _on_push_query {
+    my $self = shift;
+    unless ($self->{current_query}) {
+        my $queries = $self->{queries};
+        if (@$queries) {
+            $self->{write_watcher} = AE::io $self->{fd}, 1, sub { $self->_on_push_query_writable };
+        }
+        else {
+            _call_back($self->{on_empty_queue}, $self);
+        }
+    }
+}
+
+sub _on_push_query_writable {
+    my $self = shift;
+    delete $self->{write_watcher};
+
+    my $dbc = $self->{dbc};
+    my $query = shift @{$self->{queries}};
+    my $r = $dbc->sendQuery(_ensure_list $query->{query});
+    if ($r) {
+        $self->{current_query} = $query;
+        $self->_on_consume_input;
+    }
+    else {
+        _call_back($query->{on_error}, $self);
+        $self->_on_push_query
+    }
+}
+
+sub _on_consume_input {
+    my $self = shift;
+    my $dbc = $self->{dbc};
+    $dbc->consumeInput;
+    if ($dbc->busy) {
+        $self->{consume_input_watcher} //= AE::io $self->{fd}, 0, sub { $self->_on_consume_input }
+    }
+    else {
+        delete $self->{consume_input_watcher};
+        my $result = $dbc->result;
+        _call_back($self->{current_query}{on_result}, $self, $result);
+        undef $self->{current_query};
+        $self->_on_push_query;
+    }
+}
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-AnyEvent::Pg - Perl extension for blah blah blah
+AnyEvent::Pg - Query a PostgreSQL database asynchronously
 
 =head1 SYNOPSIS
 
   use AnyEvent::Pg;
-  blah blah blah
+  my $db = AnyEvent::Pg->new("dbname=foo",
+                             on_connect => sub { ... });
 
 =head1 DESCRIPTION
 
--- t/AnyEvent-Pg.t
+++ t/AnyEvent-Pg.t
@@ -1,15 +1,77 @@
-# Before `make install' is performed this script should be runnable with
-# `make test'. After `make install' it should work as `perl AnyEvent-Pg.t'
+#!/usr/bin/perl
 
-#########################
+use strict;
+use warnings;
+use 5.010;
 
-# change 'tests => 1' to 'tests => last_test_to_print';
+use Pg::PQ qw(:pgres);
+use AnyEvent::Pg;
 
 use Test::More tests => 1;
-BEGIN { use_ok('AnyEvent::Pg') };
 
-#########################
+sub on_connect;
+sub on_empty_queue;
+sub on_query_error;
+sub dump_result;
+sub push_query;
+my $cv = AnyEvent->condvar;
 
-# Insert your test code below, the Test::More module is use()ed here so read
-# its man page ( perldoc Test::More ) for help writing this test script.
+my $pg = AnyEvent::Pg->new('dbname=pgpqtest',
+                           on_connect => \&on_connect,
+                           on_empty_queue => \&on_empty_queue);
+
+push_query('drop table foo');
+push_query('drop table bar');
+push_query('create table foo (id int, name varstr(20))');
+
+my %data = ( hello => 10, bye => 12, hola => 45, cheers => 1);
+
+for (keys %data) {
+    push_query('insert into foo (id, name) values ($1, $2)', $data{$_}, $_);
+}
+
+push_query('select * from foo');
+
+$cv->recv;
+
+sub on_connect {
+    say 'connected!'
+}
+
+sub on_empty_query {
+    say 'queue is empty, exiting';
+    $cv->send;
+}
+
+sub push_query {
+    $pg->push_query(query => [@_],
+                    on_error => \&on_query_error,
+                    on_result => \&dump_result);
+}
+
+sub on_query_error {
+    say 'query error: ', $pg->dbc->errorMessage;
+}
+
+sub dump_result {
+    my (undef, $dbr) = @_;
+    my $dbc = $pg->dbc;
+    printf("conn status:\t'%s' (%d),\terr:\t'%s'\nresult status:\t'%s' (%d),\tmsg:\t'%s',\terr:\t'%s'\n",
+           $dbc->status, $dbc->status, $dbc->errorMessage,
+           $dbr->status, $dbr->status, $dbr->statusString, $dbr->errorMessage);
+    if ($dbr->status == PGRES_TUPLES_OK) {
+        say 'ntuples: ', $dbr->ntuples;
+        say 'nfields: ', $dbr->nfields;
+        say 'id column number: ', $dbr->fnumber('id');
+        for my $row (0 .. $dbr->nTuples - 1) {
+            for my $col (0 .. $dbr->nFields - 1) {
+                print "\t", $dbr->value($row, $col);
+            }
+            print "\n";
+        }
+    }
+}
+
+ok(1);
+
 
