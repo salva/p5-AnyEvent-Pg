31907ca9f0fcceb61c3bfbd597d4bef85a9b7094
Salvador <sfandino@yahoo.com>
2013-01-17 17:30:04 +0100

    add simple listen example

--- examples/listen.pl
+++ examples/listen.pl
@@ -17,12 +17,7 @@ my $w = $db->listen('foo',
                     on_listener_started => sub { say "started!!!" },
                     on_notify           => sub { say "foo!" });
 
-#my $qw;
-#my $tw = AE::timer 10, 10, sub { $qw = $db->push_query(query => "select now()") };
-
-warn "waiting for notification!\n";
-
+warn "waiting for notifications!\n";
 
 $cv->recv();
 
-print "watcher: $w\n";
