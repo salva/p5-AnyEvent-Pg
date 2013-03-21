4d6f525660b228de926566fe524d6280d25c4b71
Salvador <sfandino@yahoo.com>
2013-03-21 19:14:22 +0100

    prepare for release 0.09

--- lib/AnyEvent/Pg.pm
+++ lib/AnyEvent/Pg.pm
@@ -1,6 +1,6 @@
 package AnyEvent::Pg;
 
-our $VERSION = 0.08;
+our $VERSION = 0.09;
 
 use 5.010;
 use strict;
--- lib/AnyEvent/Pg/Pool.pm
+++ lib/AnyEvent/Pg/Pool.pm
@@ -1,6 +1,6 @@
 package AnyEvent::Pg::Pool;
 
-our $VERSION = '0.06';
+our $VERSION = '0.09';
 
 use strict;
 use warnings;
