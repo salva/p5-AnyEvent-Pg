3b4590799f2303c2270d7b3d722148668220d694
Pavel Shaydo <zwon@cpan.org>
2013-08-01 23:24:29 +0800

    fix misspelled attribute name
    it's queries, not queue. As result on_error callbacks for queries were
    not invoked in case of fatal errors

--- lib/AnyEvent/Pg.pm
+++ lib/AnyEvent/Pg.pm
@@ -194,9 +194,9 @@ sub _on_fatal_error {
 
     my $cq = delete $self->{current_query};
     $cq and $self->_maybe_callback($cq, 'on_error');
-    my $queue = $self->{queue};
-    $self->_maybe_callback($_, 'on_error') for @$queue;
-    @$queue = ();
+    my $queries = $self->{queries};
+    $self->_maybe_callback($_, 'on_error') for @$queries;
+    @$queries = ();
     $self->_maybe_callback('on_error', 1);
     $self->{dbc}->finish;
     delete $self->{dbc};
